66fc5d35d088c7260c7b39ec4ed0aad08f3e20da
Ilya Guseynov <rugia0094@yandex.ru>
2024-12-16 20:05:24 +0300

    add filesize limit for treesitter

--- vim/init.lua
+++ vim/init.lua
@@ -78,7 +78,17 @@ require("lazy").setup({
           ensure_installed = "all",
           sync_install = true,
           auto_instal = false,
-          highlight = { enable = true },
+          highlight = {
+            enable = true,
+            disable = function (lang, buf)
+              local max_filesize = 100 * 1024 -- 100 KB
+              local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+
+              if ok and stats and stats.size > max_filesize then
+                return true
+              end
+            end,
+          },
           indent = { enable = true },
         })
       end
